12d4d48876b0586756f5cf89b2884773417fa641
박기석 <kiseok.park@thingsflow.com>
2023-11-21 12:13:44 +0900

    feat: Range 범위 체크 연산자 오버라이딩 추가

---
+++ CommonFoundation/Sources/Extension/Foundation/Range.swift
@@ -0,0 +1,18 @@
+//
+//  Range.swift
+//  CommonFoundation
+//
+//  Created by kiseok.park on 11/21/23.
+//
+
+import Foundation
+
+public extension Range {
+    static func ~= (lhs: Self, rhs: Self) -> Bool {
+        rhs.clamped(to: lhs) == rhs
+    }
+    
+    static func ~=(lhs: Self, rhs: ClosedRange<Bound>) -> Bool {
+        lhs.lowerBound <= rhs.lowerBound && rhs.upperBound < lhs.upperBound
+    }
+}
