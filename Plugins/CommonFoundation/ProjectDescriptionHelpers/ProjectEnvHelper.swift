16462aefc80009dbe63a9c6be4ed04f1a4aa9ef5
박기석 <gkfnrkfl@naver.com>
2023-12-06 11:02:12 +0900

    feat: Tuist 환경 변수용 Plugin 추가

---
+++ Plugins/CommonFoundation/ProjectDescriptionHelpers/ProjectEnvHelper.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public struct ProjectEnvHelper {
+    public let name: String = "CommonFoundation"
+    public let organizationName: String = "kr.kiseok"
+    
+    public init() { }
+}
