06b4e288f95065287dec58979024850af331172c
박기석 <gkfnrkfl@naver.com>
2023-12-15 11:05:38 +0900

    fix: 템플릿 플러그인 경로 수정

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -2,6 +2,6 @@ import ProjectDescription
 
 let config = Config(
     plugins: [
-        .git(url: "git@github.com:Kiseok-Peter/TuistTemplate.git", sha: "66ca149")
+        .git(url: "git@github.com:Kiseok-Peter/TuistTemplate.git", sha: "e4422cd", directory: "Templates")
     ]
 )
