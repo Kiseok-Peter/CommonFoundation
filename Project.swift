4efc74e4bcfaa716208b0e37cf62ae430083f7a7
박기석 <gkfnrkfl@naver.com>
2024-10-28 20:20:13 +0900

    chore: TuistTemplate에 ModuleProtocol을 이용하여 타겟, 프로젝트 구정하도록 변경

--- Project.swift
+++ Project.swift
@@ -1,28 +1,10 @@
 import ProjectDescription
+import ProjectDescriptionHelpers
 import TuistTemplate
 
 // MARK: - Project
 
-let name = "CommonFoundation"
-let organizationName = "kr.kiseok"
+let targets = Module.allCases.map { Target(with: $0) }
 
-let targets = [Target(name: name,
-                      product: .framework,
-                      bundleName: organizationName,
-                      sources: [
-                        "\(name)/Sources/**"
-                      ]),
-               Target(name: "\(name)Tests",
-                      product: .unitTests,
-                      bundleName: organizationName,
-                      sources: [
-                        "\(name)/Tests/**"
-                      ],
-                      dependencies: [
-                        .target(name: name),
-                        .external(name: "Quick"),
-                        .external(name: "Nimble"),
-                      ])]
-
-let project = Project(name: name,
+let project = Project(name: Module.foundation.rawValue,
                       targets: targets)
---
+++ Tuist/ProjectDescriptionHelpers/Module.swift
@@ -0,0 +1,37 @@
+//
+//  Module.swift
+//  CommonFoundationManifests
+//
+//  Created by 박기석 on 10/28/24.
+//
+
+import ProjectDescription
+import TuistTemplate
+
+public enum Module: String, CaseIterable {
+    case foundation = "CommonFoundation"
+    case tests = "CommonFoundationTests"
+}
+
+extension Module: ModuleProtocol {
+    public var product: Product {
+        switch self {
+        case .foundation: .framework
+        case .tests: .unitTests
+        }
+    }
+    
+    public var infoPlist: InfoPlist { .default }
+    
+    public var resources: ResourceFileElements? { nil }
+    
+    public var dependencies: [TargetDependency] {
+        switch self {
+        case .foundation: []
+        case .tests:
+            [.target(name: Module.foundation.rawValue),
+             .external(name: "Quick"),
+             .external(name: "Nimble"),]
+        }
+    }
+}
